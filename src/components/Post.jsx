2fec0af3f38325acb890f4537231fb4f72b49ba1
Alex Singleton <alex.singleton@keyin.com>
2023-04-11 12:08:59 -0230

    users now tied to posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,13 +2,6 @@ import React from "react";
 import PostHead from "./PostHead";
 
 const Post = ({ user, date, content }) => {
-  const fetchUser = async (id) =>
-    await fetch(`http://localhost:8000/users/${id}`)
-      .then((rsp) => rsp.json())
-      .then(console.log);
-
-  fetchUser(user);
-
   return (
     <div className="post">
       <PostHead user={user} date={date} />
